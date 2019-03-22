9a5186293c658d19c6611ba99da1e6341cf34a5f
Neoteuthis <Neoteuthis@users.noreply.github.com>
2019-03-22 14:30:30 -0400

    included new fonts and more

--- js/basescript.js
+++ js/basescript.js
@@ -1,4 +1,27 @@
 var jewel = (function() { 
+
+var canvas = document.getElementById("mycanvas"),        ctx = canvas.getContext("2d");
+ 
+    canvas.width = canvas.height = 200;
+ 
+    // draw two blue circles
+    ctx.fillStyle = "blue";    ctx.beginPath();    ctx.arc(50, 50, 25, 0, Math.PI * 2, true);    ctx.arc(150, 50, 25, 0, Math.PI * 2, true);    ctx.fill();
+ 
+    // draw a red triangle    
+	ctx.fillStyle = "red";    ctx.beginPath();    ctx.moveTo(100, 75);    ctx.lineTo(75, 125);    ctx.lineTo(125, 125);    ctx.fill();
+ 
+    // draw a green semi-circle    
+	ctx.strokeStyle = "green";    ctx.beginPath();    ctx.scale(1, 0.5);    ctx.arc(100, 300, 75, Math.PI, 0, true);    ctx.closePath();    ctx.stroke(); 
+	// save highscore data 
+	localStorage.setItem("highscore", "152400");
+ 
+// data can later be retrieved, even on other pages 
+var highscore = localStorage.getItem("highscore");
+ 
+//alert(highscore); // alerts the highscore if we need it to do the thing
+	
+
+
 var scriptQueue = [],        
 numResourcesLoaded = 0,        
 numResources = 0,        
@@ -49,7 +72,6 @@ executeScriptQueue();
 }       
  };       
  image.src = src;    
-}
 
 function setup() {   }
 
@@ -57,4 +79,4 @@ function setup() {   }
   load: load,       
  setup: setup    
 };
- })(); 
+ })();
